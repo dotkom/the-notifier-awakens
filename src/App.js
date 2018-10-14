a7ca49b81ed89393ad3d43ac8e7d0dc6b0879903
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-10-14 22:26:42 +0200

    Implement the use of API controller in the App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { APIController } from './controllers';
+import { API } from './utils';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.APIController = new APIController(API);
+
+    this.startAPIs();
+  }
+
+  startAPIs() {
+    this.APIController.start();
+  }
+  stopAPIs() {
+    this.APIController.stop();
+  }
+  updateAPIs() {
+    this.APIController.update();
+  }
+
   render() {
     return <div className="App" />;
   }
