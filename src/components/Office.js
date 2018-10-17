0df70717bfc0ae9799117125e732212e33cc2af5
annemoss <anne.mosvold@oerke.net>
2018-10-17 23:43:49 +0200

    Add sceleton for office component

--- src/components/Office.js
+++ src/components/Office.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import './Office.css';
 
 export class Office extends Component {
+  constructor(){
+    super();
+    this.state = {servants:["Anne", "Malin", "Emilie", "Gunvor"]};
+  }
   render() {
-    return <h2>Office</h2>;
+    let list = this.state.servants.map((e, i) => <li key={i}>{e}</li>)
+    return (<>
+      <h2>Kontorvakt</h2>
+      <ul>
+        {list}
+      </ul>
+    </>);
   }
 }
