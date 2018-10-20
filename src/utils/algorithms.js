790487e7e38fde2a9dd8f6caa38343e60e0b070a
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-10-21 00:26:08 +0200

    Format util files

--- src/utils/algorithms.js
+++ src/utils/algorithms.js
@@ -5,9 +5,9 @@ import { get, has } from 'object-path';
 
 /**
  * List all matching object paths by using a simple string.
- * 
+ *
  * Examples:
- * 
+ *
  * object = {
  *   a: {
  *     b: {
@@ -18,18 +18,17 @@ import { get, has } from 'object-path';
  *     }
  *   }
  * }
- * 
+ *
  * findObjectPaths(object, 'a.b.c') => [ 'a.b.c' ]
  * findObjectPaths(object, 'a.*.c') => [ 'a.b.c', 'a.c.c' ]
  * findObjectPaths(object, 'a.b,c|a.c.*') => [ 'a.b', 'a.c', 'a.c.c' ]
- * 
+ *
  * @param {object} object Object to search through
  * @param {string} schema Filter to select what to return from object
- * 
+ *
  * @returns {array} Array of matching paths
  */
 export const findObjectPaths = (object, schema = '') => {
-
   // Check if a "|" is in the schema. If found, then split by "|"
   // and run findObjectPaths function over them again and avoid
   // duplicates.
@@ -88,16 +87,16 @@ export const findObjectPaths = (object, schema = '') => {
 
 /**
  * Return all matches found in a string encapsulated by substrings.
- * 
+ *
  * Examples:
  * getStringParams('test{{one}}') => [ 'one' ]
  * getStringParams('{{two}}test') => [ 'two' ]
  * getStringParams('{{three}}test{{four}}') => [ 'three', 'four' ]
- * 
+ *
  * @param {string} string The String to search through
  * @param {string} start Start of match
  * @param {string} end End of match
- * 
+ *
  * @returns {array} Array of matches
  */
 export const getStringParams = (string, start = '{{', end = '}}') => {
@@ -115,8 +114,12 @@ export const getStringParams = (string, start = '{{', end = '}}') => {
       break;
     }
 
-    result.push(string.slice(index + offsetStart, index + offsetStart + indexEnd));
-    indexStart = string.slice(index + offsetStart + indexEnd + offsetEnd).indexOf(start);
+    result.push(
+      string.slice(index + offsetStart, index + offsetStart + indexEnd),
+    );
+    indexStart = string
+      .slice(index + offsetStart + indexEnd + offsetEnd)
+      .indexOf(start);
     index += indexEnd + offsetEnd + indexStart + offsetStart;
   }
 
@@ -125,16 +128,16 @@ export const getStringParams = (string, start = '{{', end = '}}') => {
 
 /**
  * Inject values from object into string at marked locations.
- * 
+ *
  * Examples:
- * 
+ *
  * const obj = {
  *   one: 1,
  *   two: 2,
  *   three: 3,
  *   four: 4,
  * };
- * 
+ *
  * injectValuesIntoString('test{{one}}', obj) => 'test1'
  * injectValuesIntoString('{{one}}test', obj) => '1test'
  * injectValuesIntoString('test{{one}}test', obj) => 'test1test'
@@ -142,16 +145,22 @@ export const getStringParams = (string, start = '{{', end = '}}') => {
  * injectValuesIntoString('{{three}}test{{four}}', obj) => '3test4'
  * injectValuesIntoString('{{three}}test{{five}}', obj) => '3test{{five}}'
  * injectValuesIntoString('{{three}}test{{five}}', obj, '0') => '3test0'
- * 
+ *
  * @param {string} string The String to search through
  * @param {string} values An object with all values that can fit the keys
  * @param {string} fallbackValue Default value if no keys in values matches
  * @param {string} start Start of match
  * @param {string} end End of match
- * 
+ *
  * @returns {string} Generated string
  */
-export const injectValuesIntoString = (string, values, fallbackValue = null, start = '{{', end = '}}') => {
+export const injectValuesIntoString = (
+  string,
+  values,
+  fallbackValue = null,
+  start = '{{',
+  end = '}}',
+) => {
   const params = getStringParams(string, start, end);
 
   if (!params.length) {
@@ -179,7 +188,11 @@ export const injectValuesIntoString = (string, values, fallbackValue = null, sta
 
     const formattedParam = start + param + end;
     const position = string.indexOf(formattedParam, prevPosition + 1);
-    result += string.slice((prevPosition === -1 ? 0 : prevPosition) + prevFormattedParam.length, position) + value;
+    result +=
+      string.slice(
+        (prevPosition === -1 ? 0 : prevPosition) + prevFormattedParam.length,
+        position,
+      ) + value;
     prevPosition = position;
     prevFormattedParam = formattedParam;
   }
--- src/utils/algorithms.test.js
+++ src/utils/algorithms.test.js
@@ -1,4 +1,8 @@
-import { findObjectPaths, getStringParams, injectValuesIntoString } from './algorithms';
+import {
+  findObjectPaths,
+  getStringParams,
+  injectValuesIntoString,
+} from './algorithms';
 
 it('Find object paths', () => {
   const obj = {
@@ -15,35 +19,49 @@ it('Find object paths', () => {
     },
   };
 
-  expect(findObjectPaths(obj, 'a.b.c')).toEqual([ 'a.b.c' ]);
-  expect(findObjectPaths(obj, 'a.*.c')).toEqual([ 'a.b.c', 'a.c.c' ]);
-  expect(findObjectPaths(obj, '*.b')).toEqual([ 'a.b', 'b.b' ]);
-  expect(findObjectPaths(obj, '*.*')).toEqual([ 'a.b', 'a.c', 'b.b' ]);
-  expect(findObjectPaths(obj, '*.*.c')).toEqual([ 'a.b.c', 'a.c.c' ]);
-  expect(findObjectPaths(obj, '*.*.*')).toEqual([ 'a.b.c', 'a.c.c' ]);
-  expect(findObjectPaths(obj, '*.*|a.b.c')).toEqual([ 'a.b', 'a.c', 'b.b', 'a.b.c' ]);
-  expect(findObjectPaths(obj, 'a.b,c')).toEqual([ 'a.b', 'a.c' ]);
-  expect(findObjectPaths(obj, 'a.b,c.c')).toEqual([ 'a.b.c', 'a.c.c' ]);
+  expect(findObjectPaths(obj, 'a.b.c')).toEqual(['a.b.c']);
+  expect(findObjectPaths(obj, 'a.*.c')).toEqual(['a.b.c', 'a.c.c']);
+  expect(findObjectPaths(obj, '*.b')).toEqual(['a.b', 'b.b']);
+  expect(findObjectPaths(obj, '*.*')).toEqual(['a.b', 'a.c', 'b.b']);
+  expect(findObjectPaths(obj, '*.*.c')).toEqual(['a.b.c', 'a.c.c']);
+  expect(findObjectPaths(obj, '*.*.*')).toEqual(['a.b.c', 'a.c.c']);
+  expect(findObjectPaths(obj, '*.*|a.b.c')).toEqual([
+    'a.b',
+    'a.c',
+    'b.b',
+    'a.b.c',
+  ]);
+  expect(findObjectPaths(obj, 'a.b,c')).toEqual(['a.b', 'a.c']);
+  expect(findObjectPaths(obj, 'a.b,c.c')).toEqual(['a.b.c', 'a.c.c']);
   expect(findObjectPaths(obj, 'a.b,c.d')).toEqual([]);
-  expect(findObjectPaths(obj, '*.*|a.b,c.c')).toEqual([ 'a.b', 'a.c', 'b.b', 'a.b.c', 'a.c.c' ]);
+  expect(findObjectPaths(obj, '*.*|a.b,c.c')).toEqual([
+    'a.b',
+    'a.c',
+    'b.b',
+    'a.b.c',
+    'a.c.c',
+  ]);
 });
 
 it('Get string params', () => {
-  expect(getStringParams('test{{one}}')).toEqual([ 'one' ]);
-  expect(getStringParams('{{two}}test')).toEqual([ 'two' ]);
-  expect(getStringParams('{{three}}test{{four}}')).toEqual([ 'three', 'four' ]);
-  expect(getStringParams('{{five}}{{six}}')).toEqual([ 'five', 'six' ]);
-  expect(getStringParams('test{{seven}}test{{eight}}test')).toEqual([ 'seven', 'eight' ]);
-  expect(getStringParams('test{{nine}}{{ten}}')).toEqual([ 'nine', 'ten' ]);
-  expect(getStringParams('test{{eleven}}{{twelve')).toEqual([ 'eleven' ]);
-  expect(getStringParams('te}}st{{13{{14}}')).toEqual([ '13{{14' ]);
-  expect(getStringParams('te}}st{{}}')).toEqual([ '' ]);
-  expect(getStringParams('test(15)test', '(', ')')).toEqual([ '15' ]);
-  expect(getStringParams('test(16))))test', '(', '))))')).toEqual([ '16' ]);
-  expect(getStringParams('test(((17)test', '(((', ')')).toEqual([ '17' ]);
-  expect(getStringParams('test|18|test', '|', '|')).toEqual([ '18' ]);
-  expect(getStringParams('test|19|||20||', '|', '||')).toEqual([ '19', '20' ]);
-  expect(getStringParams('test||21|||22||', '|', '|||')).toEqual([ '|21' ]);
+  expect(getStringParams('test{{one}}')).toEqual(['one']);
+  expect(getStringParams('{{two}}test')).toEqual(['two']);
+  expect(getStringParams('{{three}}test{{four}}')).toEqual(['three', 'four']);
+  expect(getStringParams('{{five}}{{six}}')).toEqual(['five', 'six']);
+  expect(getStringParams('test{{seven}}test{{eight}}test')).toEqual([
+    'seven',
+    'eight',
+  ]);
+  expect(getStringParams('test{{nine}}{{ten}}')).toEqual(['nine', 'ten']);
+  expect(getStringParams('test{{eleven}}{{twelve')).toEqual(['eleven']);
+  expect(getStringParams('te}}st{{13{{14}}')).toEqual(['13{{14']);
+  expect(getStringParams('te}}st{{}}')).toEqual(['']);
+  expect(getStringParams('test(15)test', '(', ')')).toEqual(['15']);
+  expect(getStringParams('test(16))))test', '(', '))))')).toEqual(['16']);
+  expect(getStringParams('test(((17)test', '(((', ')')).toEqual(['17']);
+  expect(getStringParams('test|18|test', '|', '|')).toEqual(['18']);
+  expect(getStringParams('test|19|||20||', '|', '||')).toEqual(['19', '20']);
+  expect(getStringParams('test||21|||22||', '|', '|||')).toEqual(['|21']);
 });
 
 it('Inject values into placeholders in string', () => {
@@ -60,10 +78,20 @@ it('Inject values into placeholders in string', () => {
   expect(injectValuesIntoString('test{{one}}test', obj)).toEqual('test1test');
   expect(injectValuesIntoString('{{one}}test', obj)).toEqual('1test');
   expect(injectValuesIntoString('{{two}}test{{two}}', obj)).toEqual('2test2');
-  expect(injectValuesIntoString('{{three}}test{{four}}', obj)).toEqual('3test4');
-  expect(injectValuesIntoString('{{three}}test{{five}}', obj)).toEqual('3test{{five}}');
-  expect(injectValuesIntoString('{{three}}test{{five}}', obj, '0')).toEqual('3test0');
-  expect(injectValuesIntoString('{{five}}test{{five}}', obj)).toEqual('{{five}}test{{five}}');
-  expect(injectValuesIntoString('{{five}}test{{five}}', obj, '0')).toEqual('0test0');
+  expect(injectValuesIntoString('{{three}}test{{four}}', obj)).toEqual(
+    '3test4',
+  );
+  expect(injectValuesIntoString('{{three}}test{{five}}', obj)).toEqual(
+    '3test{{five}}',
+  );
+  expect(injectValuesIntoString('{{three}}test{{five}}', obj, '0')).toEqual(
+    '3test0',
+  );
+  expect(injectValuesIntoString('{{five}}test{{five}}', obj)).toEqual(
+    '{{five}}test{{five}}',
+  );
+  expect(injectValuesIntoString('{{five}}test{{five}}', obj, '0')).toEqual(
+    '0test0',
+  );
   expect(injectValuesIntoString('{{five}}', obj)).toEqual('{{five}}');
 });
