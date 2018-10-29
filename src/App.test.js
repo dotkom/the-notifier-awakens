7d20ad03ea996981c9f59df4e65f5cf42146a68b
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-10-29 21:34:43 +0100

    Add tests for dynamic grid layout generators

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,36 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('get correct grid-template value', () => {
+  const app = new App();
+  expect(app.getGridTemplateFromLayoutArray(['a b', 'a b'])).toEqual(
+    '"a b" "a b" / 1fr 1fr',
+  );
+  expect(app.getGridTemplateFromLayoutArray(['a a', '. b b'])).toEqual(
+    '"a a" ". b b" / 1fr 1fr 1fr',
+  );
+});
+
+it('get correct default grid-template from components', () => {
+  const app = new App();
+
+  const components = [
+    { template: 'Bus' },
+    { template: 'Clock' },
+    { template: 'Office' },
+  ];
+
+  expect(app.generateDefaultGridTemplateFromComponents(components, 1)).toEqual([
+    'Bus',
+    'Clock',
+    'Office',
+  ]);
+  expect(app.generateDefaultGridTemplateFromComponents(components, 2)).toEqual([
+    'Bus Clock',
+    'Office .',
+  ]);
+  expect(app.generateDefaultGridTemplateFromComponents(components, 3)).toEqual([
+    'Bus Clock Office',
+  ]);
+});
