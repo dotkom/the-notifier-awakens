8f3423429245d51bdd4ee9dde9d175a97f255971
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-11-04 00:23:47 +0100

    Add monika code

--- src/components/Events.js
+++ src/components/Events.js
@@ -4,7 +4,17 @@ import { format } from 'date-fns';
 
 export default class Events extends Component {
   render() {
-    const EventsUI = EventsFromSplash;
+    let EventsUI = null;
+    switch (this.props.type) {
+      case 'splash':
+        EventsUI = EventsFromSplash;
+        break;
+      case 'carousel':
+        EventsUI = EventsCarousel;
+        break;
+      default:
+        EventsUI = EventsCarousel;
+    }
     const { events = [], eventMapping } = this.props;
     const eventsMapped = events.map(e => {
       const startDateTime = get(e, eventMapping.startDate, '');
@@ -159,67 +169,24 @@ class EventsCarousel extends Component {
         <style scoped>
           {`
           .event-list {
-            display: flex;
-            flex-direction: column;
-            max-width: 800px;
-            margin: auto;
           }
           .main-event {
-            max-width: 800px;
-            margin: auto;
-            display: flex;
-            flex-direction: column;
-            height: 160px;
-            padding: 32px 24px;
-            box-sizing: border-box;
           }
           .main-title {
-            font-size: 2em;
-            color: #fff;
           }
           .main-start-datetime {
-            margin: 10px 0 0 50px;
-          }
-          .main-start-datetime::before {
-            content: '- ';
           }
           .event {
-            display: flex;
           }
           .title {
-            order: 3;
-            flex: 1 0 auto;
-            position: relative;
-            border-left: 3px solid ${lineColor};
-            padding: 10px 0 10px 42px;
           }
           .title::before {
-            content: '';
-            position: absolute;
-            right: calc(100% - 5px);
-            top: 18px;
-            background-color: ${lineColor};
-            width: 14px;
-            height: 14px;
-            border-radius: 7px;
           }
           .event:last-child .title {
-            padding-bottom: 32px;
           }
           .start-date {
-            order: 1;
-            flex: 0 0 240px;
-            text-align: right;
-            color: ${dateColor};
-            padding: 10px 0;
           }
           .start-time {
-            order: 2;
-            flex: 0 0 80px;
-            text-align: center;
-            font-size: .75em;
-            color: ${timeColor};
-            padding: 14px 10px 10px 0;
           }
           `}
         </style>
