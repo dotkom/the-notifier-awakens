7eee1e047b9c6dbfea3f8c7a6f78be3b98ee0a60
Monika Muzyk <monika.muzyk@hotmail.com>
2019-01-17 13:29:46 +0100

    Fix clock width

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -18,7 +18,7 @@ export default class Clock extends Component {
     let time = format(new Date(), 'HH:mm');
     return (
       <div>
-        <svg width="200" height="200">
+        <svg width="250" height="200">
          {/*<circle cx="50" cy="50" r="50" fill="#70186f" /> */}
           <text
             x="50%"
