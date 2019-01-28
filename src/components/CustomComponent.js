1fb93faeb9d33bdcb14c6287d7b4f4db3e3a9630
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-01-28 14:10:52 +0100

    Avoid rendering of self made templates unless there is api input

--- src/components/CustomComponent.js
+++ src/components/CustomComponent.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
 export default class CustomComponent extends Component {
+  shouldComponentUpdate(nextProps) {
+    for (const key in nextProps.apis) {
+      if (key in this.props.apis) {
+        if (nextProps[key] !== this.props[key]) {
+          return true;
+        }
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
   render() {
     return <div dangerouslySetInnerHTML={{ __html: this.props.template }} />;
   }
