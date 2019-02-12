50b4eefcd5324d77ae4f87d334e872117fe45a96
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-02-12 17:33:14 +0100

    Make Articles component use icon while loading articles

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
+import { Loading } from './';
 import './Articles.css';
 
 export default class Articles extends Component {
   render() {
-    const { articles = [] } = this.props;
+    const { articles = [], IfPropIsOnline } = this.props;
     return (
       <div className="articles">
-        {articles.map((a, i) => {
-          const style = {
-            backgroundImage: `url(${a.image})`,
-          };
-          return (
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={a.link}
-              key={i}
-              style={style}
-              className="article"
-            >
-              <span className="article-title">{a.title}</span>
-              <span className="article-author">{a.author}</span>
-            </a>
-          );
-        })}
+        <IfPropIsOnline
+          prop="articles"
+          props={this.props}
+          else={apiName => `Kunne ikke koble til ${apiName}`}
+          loading={<Loading />}
+        >
+          {articles.map((a, i) => {
+            const style = {
+              backgroundImage: `url(${a.image})`,
+            };
+            return (
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={a.link}
+                key={i}
+                style={style}
+                className="article"
+              >
+                <span className="article-title">{a.title}</span>
+                <span className="article-author">{a.author}</span>
+              </a>
+            );
+          })}
+        </IfPropIsOnline>
       </div>
     );
   }
