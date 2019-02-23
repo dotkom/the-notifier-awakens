69b3c13ac904fc5eecf2b71caeefe632bec00be6
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-02-23 02:06:44 +0100

    Abstract default settings url in constants

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,8 @@ export const API_HOST =
   process.env.REACT_APP_API_HOST || 'https://passoa.online.ntnu.no';
 export const API_ROOT = process.env.REACT_APP_API_ROOT || 'api';
 export const API_URL = `${API_HOST}/${API_ROOT}`;
+export const DEFAULT_SETTINGS_URL =
+  process.env.REACT_APP_DEFAULT_SETTINGS_URL || '';
 export const DEBUG = (process.env.REACT_APP_DEBUG || 'false') === 'true';
 const targetWhitelist = process.env.REACT_APP_TARGET_WHITELIST
   ? process.env.REACT_APP_TARGET_WHITELIST.split(',')
