b50979149f912e62aaed2bced838fa5f5746326a
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-02-23 13:05:06 +0100

    Add translations for add affiliation labels

--- src/defaults/translations.js
+++ src/defaults/translations.js
@@ -9,4 +9,9 @@ export const defaultTranslations = {
   affiliation: 'Lineforening',
   settings: 'Alternativer',
   chooseVisibility: 'Velg hva som vises',
+  chooseAffiliation: 'Velg linjeforening',
+  searchAffiliationPlaceholder: 'Søk i linjeforeninger...',
+  addAffiliation: 'Legg til linjeforening / gruppe',
+  addAffiliationInfo:
+    'Endringene som gjøres her vil bare lagres på denne enheten. Om du vil at denne skal lagres globalt kan du exportere denne som JSON og sende inn en Pull Request om endring <a href="https://github.com/dotkom/the-notifier-awakens">her</a>.',
 };
