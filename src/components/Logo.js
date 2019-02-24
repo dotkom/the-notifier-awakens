1c9e674fb186ff0b2cd4905c1e5e670c891370d3
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-02-24 19:41:06 +0100

    Make it possible to apply filter on logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,8 +3,12 @@ import './Logo.css';
 
 export default class Logo extends Component {
   render() {
+    const style = {
+      filter: this.props.filter || '',
+    };
     return (
       <img
+        style={style}
         className="logo-container"
         alt={`Logo for ${this.props.translate(this.props.affiliation)}`}
         src={this.props.url}
