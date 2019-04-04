989011faab3820fbc37804bd6a238ae16d977a8d
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-04-04 02:17:00 +0200

    Warn developers about how to disable debug mode

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter, BrowserRouter, withRouter } from 'react-router-dom';
-import { IS_CHROME_EXTENSION } from './constants';
+import { IS_CHROME_EXTENSION, DEBUG } from './constants';
+
+if (DEBUG) {
+  console.log(
+    '%cYou are using the debug mode in Notiwall. To disable, add line: `REACT_APP_DEBUG=false`, to ./.env.local',
+    'color: rgba(160, 160, 160, .5)',
+  );
+}
 
 const Root = withRouter(App);
 const Router = IS_CHROME_EXTENSION ? HashRouter : BrowserRouter;
