118cb2a9e627ac8df3fd80dd5614cae24271f0ff
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-05-02 00:51:32 +0200

    Add coffee component

---
+++ src/components/Coffee.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Icon } from './';
+
+export default class Coffee extends Component {
+  render() {
+    const {} = this.props;
+    return (
+      <>
+        <h1>Coffee</h1>
+        <div />
+      </>
+    );
+  }
+}
