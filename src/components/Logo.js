d7f2962a22d16c78c95cc58ce8a26e2cfecee904
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-11-18 20:09:33 +0100

    Make sure time works properly

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import './Logo.css';
 
 const Logo = props => {
-  const daysBefore = 0;
-  const fromTime = new Date(2019, 10, 15 - daysBefore, 0, 49, 0);
-  const toTime = new Date(2019, 10, 15, 0, 50, 0);
-  const [time, setTime] = useState(toTime - Date.now());
+  const daysBefore = 3;
+  const toTime = new Date(2019, 10, 22, 12, 0, 0).getTime();
+  const fromTime = toTime - daysBefore * 24 * 60 * 60 * 1000;
+  const [initialTime] = useState(Date.now());
+  const [time, setTime] = useState(Date.now());
   const [intervalID, setIntervalID] = useState(null);
   const dd = x => x.toLocaleString(undefined, { minimumIntegerDigits: 2 });
   const formatDate = x => {
@@ -24,11 +25,11 @@ const Logo = props => {
   const [timeFormatted, setTimeFormatted] = useState(formatDate(time));
 
   useEffect(() => {
-    if (toTime - Date.now() > 0 && fromTime - Date.now() < 0) {
+    if (toTime > Date.now()) {
       setIntervalID(
         setInterval(() => {
-          setTime(toTime - Date.now());
-        }, 100),
+          setTime(Date.now());
+        }, 1000),
       );
     }
 
@@ -40,30 +41,39 @@ const Logo = props => {
   }, []);
 
   useEffect(() => {
-    setTimeFormatted(formatDate(time));
+    if (time >= toTime && initialTime < toTime) {
+      window.location.reload();
+    }
+    setTimeFormatted(formatDate(toTime - time));
   }, [time]);
 
   const { days, hours, minutes, seconds } = timeFormatted;
 
   return (
     <div>
-      {time > 0 && days < daysBefore ? (
+      {time < toTime && time > fromTime ? (
         <div className="countdown">
           <div>
-            <div>
-              <h1>{days}</h1>
-              <p>Dag{days === 1 ? '' : 'er'}</p>
-            </div>
-            <div>
-              <h1>{dd(hours)}</h1>
-              <p>Time{hours === 1 ? '' : 'r'}</p>
-            </div>
+            {days ? (
+              <div>
+                <h1>{days}</h1>
+                <p>Dag{days === 1 ? '' : 'er'}</p>
+              </div>
+            ) : null}
+            {days || hours ? (
+              <div>
+                <h1>{dd(hours)}</h1>
+                <p>Time{hours === 1 ? '' : 'r'}</p>
+              </div>
+            ) : null}
           </div>
           <div>
-            <div>
-              <h1>{dd(minutes)}</h1>
-              <p>Minutt{minutes === 1 ? '' : 'er'}</p>
-            </div>
+            {days || hours || minutes ? (
+              <div>
+                <h1>{dd(minutes)}</h1>
+                <p>Minutt{minutes === 1 ? '' : 'er'}</p>
+              </div>
+            ) : null}
             <div>
               <h1>{dd(seconds)}</h1>
               <p>Sekund{seconds === 1 ? '' : 'er'}</p>
